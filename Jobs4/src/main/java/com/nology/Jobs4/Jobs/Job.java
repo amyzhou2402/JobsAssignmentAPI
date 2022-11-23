a914234144fd98c28a26a482216f2af35beb4c98
amyzhou2402 <61258541+amyzhou2402@users.noreply.github.com>
2022-11-23 23:29:55 +1100

    spring sec

--- Jobs4/src/main/java/com/nology/Jobs4/Jobs/Job.java
+++ Jobs4/src/main/java/com/nology/Jobs4/Jobs/Job.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -15,62 +14,70 @@ import com.nology.Jobs4.Temps.Temp;
 
 @Entity
 public class Job {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id; 
-	private String name; 
+	private Long id;
+	private String name;
 	private LocalDate startDate;
 	private LocalDate endDate;
-	
+
 	public Job(String name, LocalDate startDate, LocalDate endDate) {
 		this.name = name;
 		this.startDate = startDate;
 		this.endDate = endDate;
 	}
-	
+
 	@ManyToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "temp_id")
-	private Temp temp; 
-	
-	public Job() {}
+	private Temp temp;
+
+	public Job() {
+	}
 
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public LocalDate getStartDate() {
 		return startDate;
 	}
+
 	public void setStartDate(LocalDate startDate) {
 		this.startDate = startDate;
 	}
+
 	public LocalDate getEndDate() {
 		return endDate;
 	}
+
 	public void setEndDate(LocalDate endDate) {
 		this.endDate = endDate;
 	}
+
 	public Temp getTemp(Temp temp) {
 		// unlike normal getters this also has parameters
 		return temp;
 	}
+
 	public void setTemp(Temp temp) {
 		this.temp = temp;
 	}
+
 	public Temp findTemp() {
-		return temp; 
+		return temp;
 	}
-	
-	
-	
 
 }
